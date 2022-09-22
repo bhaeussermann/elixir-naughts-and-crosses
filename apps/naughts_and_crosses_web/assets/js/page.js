e2c0d15096c3988b621cefdbd485b77363fd0a54
Bernhard Häussermann <bernhardgh@icloud.com>
2022-09-22 21:20:19 +0200

    Remove jQuery from client-side Javascript code

--- apps/naughts_and_crosses_web/assets/js/page.js
+++ apps/naughts_and_crosses_web/assets/js/page.js
@@ -1,38 +1,42 @@
+import 'babel-polyfill';
+
 export var Page = {
   init: function() {
-    $(function() {
-      $(".cell").on("click", function() {
-        $.ajax({
-          url: $(this).data("placeUrl"),
-          success: function(response) {
-              $("#message").text(response.message);
-              response.placements.forEach(function(placement) {
-                $(".cell[data-position='" + placement.row + "," + placement.column + "']").addClass(placement.symbol)
-              });
-            }
-          });
-      });
+    addEventListener('DOMContentLoaded', function() {
+      const cells = document.getElementsByClassName('cell');
+      for (const cell of cells) {
+        const placeUrl = cell.getAttribute('data-place-url');
+        cell.addEventListener('click', async function() {
+          const response = await fetch(placeUrl);
+          const responseContent = await response.json();
+          document.getElementById('message').innerText = responseContent.message;
+          for (const placement of responseContent.placements)
+          {
+            const placementCell = document.querySelector(`.cell[data-position='${placement.row},${placement.column}']`)
+            placementCell.classList += placement.symbol;
+          }
+        });
+      }
       
-      $("#board-size").on("change", function () {
-        if (isBoardSizeValid())
-          $("#new-game-button").removeAttr("disabled");
-        else
-          $("#new-game-button").attr("disabled", "disabled");
+      const newGameButton = document.getElementById('new-game-button')
+      document.getElementById('board-size').addEventListener('change', function() {
+        newGameButton.disabled = !isBoardSizeValid();
       });
-  
-      $("#new-game-button").on("click", function() {
-        window.location.href = $(this).data("url") 
-          + "?player=" + $("#player-selection").val() + "&board-size=" + $("#board-size").val();
+
+      newGameButton.addEventListener('click', function() {
+        window.location.href = newGameButton.getAttribute('data-url')
+          + `?player=${document.getElementById('player-selection').value}&board-size=${document.getElementById('board-size').value}`;
       });
     });
   }
 }
 
 function isBoardSizeValid() {
-  var boardSize = parseInt($("#board-size").val())
+  const boardSizeElement = document.getElementById('board-size');
+  var boardSize = parseInt(boardSizeElement.value)
   if (isNaN(boardSize))
     return false;
     
-  var min = parseInt($("#board-size").attr("min")), max = parseInt($("#board-size").attr("max"));
+  var min = parseInt(boardSizeElement.getAttribute('min')), max = parseInt(boardSizeElement.getAttribute('max'));
   return (min <= boardSize && boardSize <= max);
 }

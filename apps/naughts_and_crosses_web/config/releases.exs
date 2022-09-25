fc1fa7de56e6445bc2280f0fc47e160c9fef5005
Bernhard Häussermann <bernhardgh@icloud.com>
2022-09-25 17:56:47 +0200

    Fix deployment on Render

--- apps/naughts_and_crosses_web/config/releases.exs
+++ apps/naughts_and_crosses_web/config/releases.exs
@@ -1,4 +1,6 @@
 import Config
 
 config :naughts_and_crosses_web, NaughtsAndCrossesWeb.Endpoint,
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: secret_key_base,
   server: true
